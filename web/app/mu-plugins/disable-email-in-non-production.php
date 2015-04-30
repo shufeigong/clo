00ee2ccbdf7e92e91b1cab2304e0f46c472f4142
Marty Penner <marty@gotenzing.com>
2015-04-30 12:45:36 -0400

    Give up on sending password reset emails in non-production

--- web/app/mu-plugins/disable-email-in-non-production.php
+++ web/app/mu-plugins/disable-email-in-non-production.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name: Disable Email In Non-Production Environments
- * Description: Disable sending email in non-production environments, except for password reset emails.
+ * Description: Disable sending email in non-production environments.
  * Version: 1.0
  * Author: Marty Penner
  */
@@ -11,22 +11,10 @@ if (!function_exists('wp_mail') && getenv('WP_ENV') !== 'production') {
     /**
      * Fake sending an email.
      *
-     * @param string|array $to
-     * @param string       $subject
-     * @param string       $message
-     * @param string       $headers
-     * @param array        $attachments
-     *
      * @return bool
      */
-    function wp_mail($to, $subject, $message, $headers = '', $attachments = [])
+    function wp_mail(/* $to, $subject, $message, $headers = '', $attachments = [] */)
     {
-        if (false !== stripos($subject, 'Password Reset')) {
-            $sendSuccessful = wpMandrill::mail($to, $subject, $message, $headers, $attachments);
-
-            return $sendSuccessful;
-        }
-
         return true;
     }
 }
