844f2981994e3b8102b88984f8c196450cefb6bd
Marty Penner <marty@gotenzing.com>
2015-04-30 12:19:03 -0400

    Allow password reset emails to be sent regardless of environment and send mandrill email through a subaccount

--- web/app/mu-plugins/disable-email-in-non-production.php
+++ web/app/mu-plugins/disable-email-in-non-production.php
@@ -2,6 +2,7 @@
 
 /**
  * Plugin Name: Disable Email In Non-Production Environments
+ * Description: Disable sending email in non-production environments, except for password reset emails.
  * Version: 1.0
  * Author: Marty Penner
  */
@@ -10,10 +11,22 @@ if (!function_exists('wp_mail') && getenv('WP_ENV') !== 'production') {
     /**
      * Fake sending an email.
      *
+     * @param string|array $to
+     * @param string       $subject
+     * @param string       $message
+     * @param string       $headers
+     * @param array        $attachments
+     *
      * @return bool
      */
-    function wp_mail()
+    function wp_mail($to, $subject, $message, $headers = '', $attachments = [])
     {
+        if (false !== stripos($subject, 'Password Reset')) {
+            $sendSuccessful = wpMandrill::mail($to, $subject, $message, $headers, $attachments);
+
+            return $sendSuccessful;
+        }
+
         return true;
     }
 }
---
+++ web/app/mu-plugins/send-mandrill-email-through-subaccount.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tenzing\WordPress;
+
+/**
+ * Plugin Name: Send Mandrill Email Through Subaccount
+ * Version: 1.0
+ * Author: Marty Penner
+ */
+
+add_filter('mandrill_payload', __NAMESPACE__.'\sendEmailThroughMandrillSubaccount', 100);
+/**
+ * Send all email that Mandrill is handling through a subaccount.
+ *
+ * @param array $message
+ *
+ * @return array
+ */
+function sendEmailThroughMandrillSubaccount(array $message)
+{
+    $message['subaccount'] = getenv('MANDRILL_SUBACCOUNT');
+
+    return $message;
+}
