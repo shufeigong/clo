154d0a413abf0f1bfee8b9529082a369a5fcc510
Marty Penner <marty@gotenzing.com>
2015-05-01 12:23:40 -0400

    Ignore cache config to allow it to be auto-generated

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,7 @@ set :log_level, :info
 # it needs to be added to linked_files so it persists across deploys:
 #set :linked_files, fetch(:linked_files, []).push('.env', 'web/.htaccess')
 set :linked_files, fetch(:linked_files, []).push('.env')
+set :linked_dirs, fetch(:linked_dirs, []).push('web/app/wp-cache-config.php')
 set :linked_dirs, fetch(:linked_dirs, []).push('web/app/uploads')
 set :linked_dirs, fetch(:linked_dirs, []).push('web/app/cache')
 
