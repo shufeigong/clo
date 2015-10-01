8281af15fb665fc0ad35880309008cc3dbfd9cc8
Shufei <shufei@gotenzing.com>
2015-10-01 15:32:28 -0400

    Set scroll post responsive and resize end

--- web/app/themes/clo_web_2015/src/js/scrollPost.js
+++ web/app/themes/clo_web_2015/src/js/scrollPost.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
-	
-	setScrollPost();
-	
+	setScrollPost();	
+});
 
-	
-   
+
+
+
+$(window).resizeEnd({delay: 500}, function(){
+	setScrollPost2();
 });
 
 function setScrollPost(){
@@ -20,26 +22,43 @@ function setScrollPost(){
 	//var textUp2 = textDat.substr(0,br);
 	textDiv.innerHTML = textDat+textDat+textDat.substr(0,br);
 	textDiv.style.cssText = "position:absolute; top:0";
-	var textDatH = textDiv.offsetHeight;MaxRoll();
+	var textDatH = textDiv.offsetHeight;
+	MaxRoll();
 	}
 	var minTime,maxTime,divTop,newTop=0;
+	
 	function MinRoll(){
-	newTop++;
-	if(newTop<=divTop+2*singleheight){
-	textDiv.style.top = "-" + newTop + "px";
-	}else{
-	clearInterval(minTime);
-	maxTime = setTimeout(MaxRoll,5000);
-	}
+	 newTop++;
+	 if(newTop<=divTop+2*singleheight){
+	   textDiv.style.top = "-" + newTop + "px";
+	  }else{
+	   clearInterval(minTime);
+	   maxTime = setTimeout(MaxRoll,5000);
+	  }
 	}
 	function MaxRoll(){
-	divTop = Math.abs(parseInt(textDiv.style.top));
-	if(divTop>=0 && divTop<textDatH-2*singleheight){
-	minTime = setInterval(MinRoll,1);
-	}else{
-	textDiv.style.top = 0;divTop = 0;newTop=0;MaxRoll();
-	}
+	  divTop = Math.abs(parseInt(textDiv.style.top));
+	  if(divTop>=0 && divTop<textDatH-2*singleheight){
+	   minTime = setInterval(MinRoll,1);
+	  }else{
+	   textDiv.style.top = 0;divTop = 0;newTop=0;MaxRoll();
+	  }
 	}
 	
 	$('.slvj-link-lightbox').simpleLightboxVideo();
+}
+
+
+function setScrollPost2(){
+	var singleheight = $("#rollPost").children("li").height()+10;
+	
+	$("#rollArea").css({"height":2*singleheight});
+	
+	var textDiv2 = document.getElementById("rollPost");
+	var textList2 = textDiv2.getElementsByTagName("li");
+	if(textList2.length > 2){
+	textDiv2.style.cssText = "position:absolute; top:0";
+	}
+	
+	//$('.slvj-link-lightbox').simpleLightboxVideo();
 }
