0c6806d8ef02f73967de661dd8029428db09cbcf
Insu Mun <insu@gotenzing.com>
2015-10-07 09:50:38 -0400

    Change the favicon urls.

--- web/app/themes/clo_web_2015/header.php
+++ web/app/themes/clo_web_2015/header.php
@@ -15,22 +15,18 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/src/img/icons/favicon.ico"
+    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/icons/favicon.ico"
           type="image/x-icon">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_stylesheet_directory_uri(
-    ); ?>/src/img/icons/apple-touch-icon-144x144-precomposed.png">
+    ); ?>/dist/img/icons/apple-touch-icon-144x144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(
-    ); ?>/src/img/icons/apple-touch-icon-114x114-precomposed.png">
+    ); ?>/dist/img/icons/apple-touch-icon-114x114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72"
-          href="<?php echo get_stylesheet_directory_uri(); ?>/src/img/icons/apple-touch-icon-72x72-precomposed.png">
+          href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/icons/apple-touch-icon-72x72-precomposed.png">
     <link rel="apple-touch-icon-precomposed"
-          href="<?php echo get_stylesheet_directory_uri(); ?>/src/img/icons/apple-touch-icon-precomposed.png">
-          
-          
-    <?php wp_head(); ?>
-    
-    
+          href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/icons/apple-touch-icon-precomposed.png">
 
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <?php do_action('foundationpress_after_body'); ?>
@@ -44,10 +40,6 @@
         </nav>
 
      <?php get_template_part('parts/off-canvas-menu'); ?>
-     
-<!--        -->
-     
- 
 
 <section class="container" role="document">
 <?php do_action('foundationpress_after_header'); ?>
@@ -55,17 +47,9 @@
     <div class="row header">
         <div class="columns large-4 medium-4 small-12 small-logo">
         <?php if(ICL_LANGUAGE_CODE=='en'): ?>
-            <a class="site-logo" href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>">
-
-<!--                <img class="" alt="Community Living Ontario Logo" src="--><?php //echo get_stylesheet_directory_uri(); ?><!--/dist/img/clo_logo.svg" />-->
-<!--                <img class="show-for-medium-only" alt="Community Living Ontario Logo" src="--><?php //echo get_stylesheet_directory_uri(); ?><!--/dist/img/clo_logo_medium.svg" />-->
-            </a>
-        <?php elseif(ICL_LANGUAGE_CODE=='fr'): ?>    
-        <a class="site-logo-french" href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>">
-
-<!--                <img class="" alt="Community Living Ontario Logo" src="--><?php //echo get_stylesheet_directory_uri(); ?><!--/dist/img/clo_logo.svg" />-->
-<!--                <img class="show-for-medium-only" alt="Community Living Ontario Logo" src="--><?php //echo get_stylesheet_directory_uri(); ?><!--/dist/img/clo_logo_medium.svg" />-->
-            </a>
+            <a class="site-logo" href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"></a>
+        <?php elseif(ICL_LANGUAGE_CODE=='fr'): ?>
+        <a class="site-logo-french" href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"></a>
          <?php endif;?>   
         </div>
         <div class="columns large-8 medium-4 small-8 small-utility">
