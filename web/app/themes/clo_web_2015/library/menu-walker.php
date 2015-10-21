b8e613a76a52264f4971f1336fc8ac37cd54c77a
Shufei <shufei@gotenzing.com>
2015-10-21 12:59:13 -0400

    Remove title attribute for accessibility

--- web/app/themes/clo_web_2015/library/menu-walker.php
+++ web/app/themes/clo_web_2015/library/menu-walker.php
@@ -135,8 +135,8 @@ class Main_Nav_walker extends Walker_Nav_Menu
 		
     
 		// link attributes
-		$attributes = !empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
-		$attributes .= !empty($item->target) ? ' target="' . esc_attr($item->target) . '"' : '';
+		//$attributes = !empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
+		$attributes = !empty($item->target) ? ' target="' . esc_attr($item->target) . '"' : '';
 		$attributes .= !empty($item->xfn) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
 		$attributes .= !empty($item->url) ? ' href="' . esc_attr($item->url) . '"' : '';
 		$attributes .= 'onclick="return false"';
@@ -450,8 +450,8 @@ class MainMobile_Nav_walker extends Walker_Nav_Menu
 		}
 		
 		// link attributes
-		$attributes = !empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
-		$attributes .= !empty($item->target) ? ' target="' . esc_attr($item->target) . '"' : '';
+		//$attributes = !empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
+		$attributes = !empty($item->target) ? ' target="' . esc_attr($item->target) . '"' : '';
 		$attributes .= !empty($item->xfn) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
 		$attributes .= !empty($item->url) ? ' href="' . esc_attr($item->url) . '"' : '';
 		//$attributes .= ' href="#" onclick="return false"';
