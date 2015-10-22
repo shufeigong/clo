731c980f32c9137fee59ddb5295326ae10f93ae0
Shufei <shufei@gotenzing.com>
2015-10-22 09:21:55 -0400

    Add label for searchbutton

--- web/app/themes/clo_web_2015/searchform.php
+++ web/app/themes/clo_web_2015/searchform.php
@@ -18,6 +18,7 @@ do_action( 'foundationpress_before_searchform' ); ?>
 
         <?php do_action('foundationpress_searchform_before_search_button'); ?>
 
+        <label for="searchbutton" class="hidden">Search button</label>
         <input id="searchbutton" type="image" alt="Search"
                src="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/search_icon.svg"/>
 
